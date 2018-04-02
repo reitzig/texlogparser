55ac97ab6fd6b66ad38eaf356015e2e50ab8eac5
Raphael Reitzig <4246780+reitzig@users.noreply.github.com>
2018-04-02 13:23:05 +0200

    Adds subts for upcoming message patterns.

---
+++ lib/texlogparser/patterns/bad_hbox_warning.rb
@@ -0,0 +1,8 @@
+require "#{File.expand_path(__dir__)}/../log_pattern"
+
+# TODO: document
+class BadHboxWarning
+  include RegExpPattern
+
+  # TODO: implement
+end
---
+++ lib/texlogparser/patterns/runaway_parameter_error.rb
@@ -0,0 +1,8 @@
+require "#{File.expand_path(__dir__)}/../log_pattern"
+
+# TODO: document
+class RunawayParameterError
+  include RegExpPattern
+
+  # TODO: implement
+end
